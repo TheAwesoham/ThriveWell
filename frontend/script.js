868be5937fd58ebe078e62cd76d1a6df1e139ffc
Soham Sarkar <121566324+TheAwesoham@users.noreply.github.com>
2025-10-25 16:47:17 +0530

    Update script.js
    Removed sensitive API key from script.

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ const fileCancelButton = document.querySelector("#file-cancel");
 const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
-const API_KEY = "AIzaSyDwAEGN9hxddTpLNer1lGRYFbpxjxejnVM";
+const API_KEY = "";
 const API_URL =`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
