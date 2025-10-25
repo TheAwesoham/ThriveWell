6dedfaa959698d6f36f3d2ac620368db44568097
Soham Sarkar <121566324+TheAwesoham@users.noreply.github.com>
2025-10-25 16:49:20 +0530

    Initial commit
    Added a comment to indicate where to insert the API key.

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ const fileCancelButton = document.querySelector("#file-cancel");
 const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
-const API_KEY = "";
+const API_KEY = ""; //add your API key
 const API_URL =`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
